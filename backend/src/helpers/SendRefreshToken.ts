5aa07661744be3c621e1873489461645ae2cf782
backstageproctt-maker <backstagepro.ctt@gmail.com>
2026-04-25 21:04:44 -0300

    Fix: Configurando cookie de sessão para Cross-Site (Vercel -> Render)

--- backend/src/helpers/SendRefreshToken.ts
+++ backend/src/helpers/SendRefreshToken.ts
@@ -1,5 +1,9 @@
 import { Response } from "express";
 
 export const SendRefreshToken = (res: Response, token: string): void => {
-  res.cookie("jrt", token, { httpOnly: true });
+  res.cookie("jrt", token, { 
+    httpOnly: true,
+    secure: true, // Necessário para HTTPS e Cross-Site
+    sameSite: "none" // Permite que a Vercel fale com o Render
+  });
 };
