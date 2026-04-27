c75d43ef59107dcbd125b31177d420d31f11de74
backstageproctt-maker <backstagepro.ctt@gmail.com>
2026-04-27 01:06:52 -0300

    fix: Enabled root socket connection in SocketWorker

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -2,9 +2,14 @@ import openSocket from "socket.io-client";
 import { isObject } from "lodash";
 import SocketWorker from "./SocketWorker"
 
-export function socketConnection(params) {
+export function socketConnection(params, unused, isRoot = false) {
   let userId = "";
   let companyId = "";
+  
+  if (isRoot) {
+    return SocketWorker(null, null); // Conecta ao root
+  }
+
   if (isObject(params)){
     companyId = params?.user?.companyId
     userId = params?.user?.id
